306ffda837c30c3b194abcabdc027e871d32958e
Akshat Sinha <akshatsinhasramhardy@gmail.com>
2025-04-18 01:18:07 +0530

    Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component'; // <-- Import FooterComponent
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet], // <-- Add FooterComponent here
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
